aec683d1aa8a56fbf7c7b7c02852dbf9fcf2204f
Bryan <bryan_unx@Jeff>
2024-09-24 00:38:10 -0400

    added operations

---
+++ tests/test_operations.py
@@ -0,0 +1,25 @@
+'''My Calculator Test'''
+import pytest
+from app.opperations import addition, division, multiplication, subtraction
+
+def test_addition():
+    '''Addition function'''
+    assert addition(1, 1) == 2
+
+def test_subtraction():
+    '''Subtraction function'''
+    assert subtraction(1, 1) == 0
+
+def test_multiplication():
+    '''Multiplication function'''
+    assert multiplication(1, 2) == 2
+
+def test_positive_division():
+    '''Positive Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10,0)
+
+def test_negative_division():
+    '''Negative Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10,0)
